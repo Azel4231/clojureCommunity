42ff5f58f1df8af07b5ad0ac5cd1d59391b185f9
Azel4231 <Azel4231@users.noreply.github.com>
2017-05-10 09:17:50 +0200

    My solution

--- labyrinth/src/labyrinth/solution.clj
+++ labyrinth/src/labyrinth/solution.clj
@@ -1,3 +1,38 @@
 (ns labyrinth.solution)
 
-(defn solve [labyrinth])
+(def walkable? #{:_ :Exit})
+(defn can-go? [direction lab pos]
+  (walkable? (get-in lab (direction pos))))
+(defn up [[x y]]
+  [x (dec y)])
+(defn down [[x y]]
+  [x (inc y)])
+(defn left [[x y]]
+  [(dec x) y])
+(defn right [[x y]]
+  [(inc x) y])
+
+(defn walk [lab pos]
+  (->> [up down left right]
+       (filter #(can-go? % lab pos))
+       (mapcat #(solutions-at lab (% pos)))))
+
+(defn solutions-at [lab pos]
+  (if (= :Exit (get-in lab pos))
+    [lab]
+    (walk (update-in lab pos (fn [_] 1)) pos)))
+
+(defn solve [lab]
+  (let [solutions (solutions-at lab [0 0])]
+    (if (empty? solutions) ["no way out"]
+        (first solutions))))
+
+(solutions-at [[1  :W :Exit]
+               [:_ :_ :_]
+               [:_ :W :_]
+               [:_ :_ :_]] [0 0])
+
+(comment "Doesn't find the starting position. Assumes [0 0]"
+         "Doesn't mark :Exit with 1"
+         "solutions-at returns a lazy sequence of all solutions") 
+
