f44884b42ed02f16ab7218dfb6297eda698f20d8
Azel4231 <Azel4231@users.noreply.github.com>
2017-05-12 08:00:37 +0200

    Switched to vector addition, renamed to maze

--- labyrinth/src/labyrinth/solution.clj
+++ labyrinth/src/labyrinth/solution.clj
@@ -1,38 +1,36 @@
 (ns labyrinth.solution)
 
 (def walkable? #{:_ :Exit})
-(defn can-go? [direction lab pos]
-  (walkable? (get-in lab (direction pos))))
-(defn up [[x y]]
-  [x (dec y)])
-(defn down [[x y]]
-  [x (inc y)])
-(defn left [[x y]]
-  [(dec x) y])
-(defn right [[x y]]
-  [(inc x) y])
 
-(defn walk [lab pos]
+(defn can-go? [direction maze pos]
+  (walkable? (get-in maze (direction pos))))
+
+(defn vec+ [& vs]
+  (apply (partial map +) vs))
+
+(defn up [pos] (vec+ pos [0 -1]))
+(defn down [pos] (vec+ pos [0 1]))
+(defn left [pos] (vec+ pos [-1 0]))
+(defn right [pos] (vec+ pos [1 0]))
+
+(declare solutions-at)
+
+(defn walk [maze pos]
   (->> [up down left right]
-       (filter #(can-go? % lab pos))
-       (mapcat #(solutions-at lab (% pos)))))
+       (filter #(can-go? % maze pos))
+       (mapcat #(solutions-at maze (% pos)))))
 
-(defn solutions-at [lab pos]
-  (if (= :Exit (get-in lab pos))
-    [lab]
-    (walk (assoc-in lab pos 1) pos)))
+(defn solutions-at [maze pos]
+  (if (= :Exit (get-in maze pos))
+    [maze]
+    (walk (assoc-in maze pos 1) pos)))
 
-(defn solve [lab]
-  (let [solutions (solutions-at lab [0 0])]
+(defn solve [maze]
+  (let [solutions (solutions-at maze [0 0])]
     (if (empty? solutions) ["no way out"]
         (first solutions))))
 
-(solutions-at [[1  :W :Exit]
-               [:_ :_ :_]
-               [:_ :W :_]
-               [:_ :_ :_]] [0 0])
-
-(comment "Doesn't find the starting position. Assumes [0 0]"
-         "Doesn't mark :Exit with 1"
-         "solutions-at returns a lazy sequence of all solutions") 
-
+(solutions-at [[1 :W :_ :_ :_]
+               [:_ :W :_ :W :_]
+               [:_ :_ :_ :W :Exit]
+               [:_ :W :_ :_ :_]] [0 0])
